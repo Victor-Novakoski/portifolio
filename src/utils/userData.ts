25182bc4e71380f7a7d8940d11a4a02c077e022b
Victor Novakoski <Novakoski2021@gmail.com>
2023-04-16 11:03:14 -0300

    fix: link do linkedin arrumado

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Victor Henrique",
-  githubUser: "Victor-Novakoski",
-  linkedinUser: "Victor Novakoski",
-  whatsappNumber: "+5541995996176",
-  emailUser: "novakoski2021@gmail.com",
-  instagramUser: "",
-  facebookUser: "",
-};
+  nameUser: 'Victor Henrique',
+  githubUser: 'Victor-Novakoski',
+  linkedinUser: 'victor-novakoski-6b0163240/',
+  whatsappNumber: '+5541995996176',
+  emailUser: 'novakoski2021@gmail.com',
+  instagramUser: '',
+  facebookUser: '',
+}
